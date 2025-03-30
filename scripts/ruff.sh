4d206bb0696877550d225663e75bc1fa2cbcd66b
Totto16 <tobiausgais@gmail.com>
2025-03-30 18:43:41 +0200

    fix: try to fix format error (only present in CI)

--- scripts/ruff.sh
+++ scripts/ruff.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-uv sync --no-editable --group lint --group own --group test
+uv sync --no-editable --group lint --group own --group test --group dev
 uv run --no-editable ruff check src/ tests/ "$@"
